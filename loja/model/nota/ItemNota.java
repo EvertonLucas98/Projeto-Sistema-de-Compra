084822b9cfd299c65e9b2da110f354908b5f4d49
Rafael Rocha <94268948+olfrech@users.noreply.github.com>
2025-07-22 10:11:25 -0300

    Update ItemNota.java
    adicionado o corpo do arquivo ItemNota.java

--- loja/model/nota/ItemNota.java
+++ loja/model/nota/ItemNota.java
@@ -1,5 +1,29 @@
 package loja.model.nota;
+import java.math.BigDecimal;
 
 public class ItemNota {
-    
+    private String nome;
+    private int quantidade;
+    private BigDecimal price;
+    private BigDecimal subTotal;
+
+    public ItemNota(String nome, int quantidade, BigDecimal price){
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.price = price;
+        this.subTotal = BigDecimal.valueOf(quantidade).multiply(precoAtual);
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public int getQtd(){
+        return quantidade;
+    }
+    public BigDecimal getPrice(){
+        return price;
+    }
+    public BigDecimal getSubTotal(){
+        return subTotal;
+    }
 }
